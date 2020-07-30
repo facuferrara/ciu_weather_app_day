1759eeed4f3721c901a958e163766fe64c2c09e6
Facundo Ferrara <facuferrarahotmail.com>
2020-07-29 23:18:12 -0300

    detalles

--- obtenerClima.js
+++ obtenerClima.js
@@ -12,9 +12,11 @@ function obtenerElClimaActual() {
 
   return fetch(request)
     .then(response => response.json()) //Consumiendo  API.
-    .then(informacionActual => {        
-      var datoActual = new Date() 
-      //Hora
+    .then(informacionActual => {   
+      //Permite trabajar con fechas y horas.     
+      var datoActual = new Date()  
+
+      //Hora  con substr le quito las cosas innecesarias/que estan de sobra en el string
       document.getElementById("date").innerHTML =  datoActual.toString().substr(0,21);
     
       //Temperaturas (actuales-current)
@@ -25,14 +27,11 @@ function obtenerElClimaActual() {
         document.getElementById("tempMax").innerHTML = `${parseInt(informacionActual.daily[0].temp.max)}°C↑`;
         document.getElementById("tempMin").innerHTML = `${parseInt(informacionActual.daily[0].temp.min)}°C↓`;
 
-      // Iconos  
-        const icono = informacionActual.current.weather[0].icon;
-        const url = ` https://openweathermap.org/img/wn/${icono}@4x.png`;
-        document.getElementById("sunnyImg").innerHTML = `<img src=${url}>`;
+      // Sunny Texto  
         document.getElementById("sunnyText").innerHTML = informacionActual.current.weather[0].main; 
 
       //humedad
-      document.getElementById("humidityText1").innerHTML =  `${informacionActual.daily[0].humidity}%`;
+      document.getElementById("humidityText1").innerHTML =  `${informacionActual.current.humidity}%`;
       
       //Presion (pressure)
       document.getElementById("textPressure").innerHTML = `${parseInt(informacionActual.daily[0].pressure)}mBar`;
@@ -44,33 +43,33 @@ function obtenerElClimaActual() {
       const sunriseActual = new Date(informacionActual.daily[0].sunrise);
       document.getElementById("sunriseHs").innerHTML = `${sunriseActual.getHours()}:${sunriseActual.getMinutes()}`;
 
-      //Puesta de sol (Mediodia-Sunset) se actualiza cada dia (daily)
+      //Puesta de sol (atardecer-Sunset) se actualiza cada dia (daily)
       const sunsetActual = new Date(informacionActual.daily[0].sunset * 1000);
       document.getElementById("sunsetHs").innerHTML = `${sunsetActual.getHours()}:${sunsetActual.getMinutes()}`;
       
       
-      //Tiempo del dia (Duracion-Daytime) se actualiza cada hora(hourly)
-      const daytimeActual= new Date(informacionActual.hourly[0].dt * 1000);
+      //Tiempo del dia (Duracion-Daytime) se actualiza cada hora
+      const daytimeActual= new Date(informacionActual.current.dt);
       document.getElementById("dayTimeHs").innerHTML = `${daytimeActual.getHours()}`;
 
+      //Siguiente dia
+      const daytimeMon= new Date(informacionActual.current.dt);
+      document.getElementById("mon21").innerHTML =  datoActual.toString().substr(0,10);
 
 
-      //Siguientes dias 
+      //Actualizacion de Dias  dentro de los proximos 4 
         const mañana = new Date(datoActual);
           for ( i = 1; i <= 4; i ++) { 
-            //siempre son 4 dias, entonces el recorrido se ejecuta
-              const icon = informacionActual.daily[index].weather[0].icon; //traigo los iconos
-              const url = ` https://openweathermap.org/img/wn/${icon}@2x.png`; 
               
-              document.getElementById(`sunnyImg${i}`).innerHTML = `<img src=${url}>`;
-              document.getElementById(`tempMax${i}`).innerHTML = `${parseInt(informacionActual.daily[i].temp.max)}°C↑`;
-              document.getElementById(`tempMin${i}`).innerHTML = `${parseInt(informacionActual.daily[i].temp.min)}°C↓`;
+              document.getElementById(`tempMax${i}`).innerHTML = `${parseInt(informacionActual.daily[i].temp.max)}`;
+              document.getElementById(`tempMin${i}`).innerHTML = `${parseInt(informacionActual.daily[i].temp.min)}`;
               
-              mañana.setDate(datoActual.getDate() + i);
+              
+              mañana.setDate(datoActual.getDate() + i);//Se actualizan los dias
               document.getElementById(`currentHour${i}`).innerHTML = `
-                
-              ${mañana.toString().substr(0,3)}, 
-              ${mañana.toString().substr(7,3)}`
+                ${mañana.toString().substr(0,3)},
+                ${mañana.toString().substr(7,3)}`
+              
         }
 
       return informacionActual.daily[0] //Devuelve dia actual.
